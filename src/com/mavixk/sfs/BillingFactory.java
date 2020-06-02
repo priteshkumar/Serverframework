9057a0bc22688aa468e92c391f025058e5fdc061
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-06-02 12:42:52 +0530

    add factory pattern coolui example

---
+++ src/com/mavixk/sfs/BillingFactory.java
@@ -0,0 +1,61 @@
+package com.mavixk.sfs;
+
+public class BillingFactory {
+
+  static interface Bill {
+
+    void getBill();
+  }
+
+  static class AirFreightBill implements Bill {
+
+    private int amount;
+
+    public AirFreightBill(int amount) {
+      this.amount = amount;
+    }
+
+    public void getBill() {
+      System.out.println("this is air freight bill");
+    }
+  }
+
+  static class SeaFreightBill implements Bill {
+
+    private int amount;
+
+    public SeaFreightBill(int amount) {
+      this.amount = amount;
+    }
+
+    public void getBill() {
+      System.out.println("this is sea freight bill");
+    }
+  }
+
+  static class Factory {
+
+    public static Bill getBill(String billType) {
+      Bill bill = null;
+      switch (billType) {
+        case "air":
+          bill = new AirFreightBill(20);
+          break;
+        case "sea":
+          bill = new SeaFreightBill(30);
+          break;
+        default:
+          System.out.println("no matching bill type");
+      }
+      return bill;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(System.getProperty("os.name"));
+    Bill bill = Factory.getBill("air");
+    bill.getBill();
+    bill = Factory.getBill("sea");
+    bill.getBill();
+  }
+}
---
+++ src/com/mavixk/sfs/CoolUI.java
@@ -0,0 +1,100 @@
+package com.mavixk.sfs;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class CoolUI {
+
+  static interface Button{
+    void render();
+    void onClick();
+  }
+
+  static class LinuxButton implements Button{
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame();
+    JButton button;
+    @Override
+    public void render() {
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JLabel label = new JLabel("Hello World!");
+      label.setOpaque(true);
+      label.setBackground(new Color(235, 233, 126));
+      label.setFont(new Font("Dialog", Font.BOLD, 44));
+      label.setHorizontalAlignment(SwingConstants.CENTER);
+      panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+      frame.getContentPane().add(panel);
+      panel.add(label);
+      onClick();
+      panel.add(button);
+
+      frame.setSize(320, 200);
+      frame.setVisible(true);
+//      onClick();
+    }
+
+    @Override
+    public void onClick() {
+      button = new JButton("Exit");
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          frame.setVisible(false);
+          System.exit(0);
+        }
+      });
+    }
+  }
+
+  static class WebButton implements Button{
+
+    @Override
+    public void render() {
+      System.out.println("<button>Test</button>");
+      onClick();
+    }
+
+    @Override
+    public void onClick() {
+      System.out.println("test clicked");
+    }
+  }
+
+  static abstract class Dialog{
+    public abstract Button createButton();
+    public void renderWindow(){
+      Button button = createButton();
+      button.render();
+    }
+  }
+
+  static class LinuxDialog extends Dialog{
+    @Override
+    public Button createButton() {
+      return new LinuxButton();
+    }
+  }
+
+  static class WebDialog extends Dialog{
+    @Override
+    public Button createButton() {
+      return new WebButton();
+    }
+  }
+
+  public static void main(String[] args){
+    Dialog dialog = null;
+    if(System.getProperty("os.name").equals("Linux"))
+      dialog = new LinuxDialog();
+    else
+      dialog = new WebDialog();
+    dialog.renderWindow();
+  }
+}
